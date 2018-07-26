8fe37ec459dfed09d6b67876e1b94d28261793d7
dani <daniboo83@gmail.com>
2018-07-26 19:38:02 +0100

    DRY things up via methods

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'Enter players:' do
   scenario 'submitting names' do
-    visit('/')
-    fill_in :player_1_name, with: 'Havelock'
-    fill_in :player_2_name, with: 'Margolotta'
-    click_button 'Done!'
+    sign_in_and_play
     expect(page).to have_content 'Havelock vs. Margolotta'
   end
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'View hit points:' do
   scenario 'see Player 2\'s hit points' do
-    visit('/')
-    fill_in :player_1_name, with: 'Havelock'
-    fill_in :player_2_name, with: 'Margolotta'
-    click_button 'Done!'
+    sign_in_and_play
     expect(page).to have_content 'Margolotta: 100HP'
   end
 end
